dd49bf4cbdd810e6003619ac355ac2b0ea458cfb
Vitaliy Balabanov <tertiumnon@gmail.com>
2020-12-03 22:01:19 +0300

    init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  wishers: string[];
+  wishersInput: string;
+  wishersPairs: string[];
+  wishersPairsInput: string;
 
-  constructor(private router: Router) { }
-
-  ngOnInit(): void { }
+  randomize(): void {
+    this.wishers = this.wishersInput.split('\n');
+    this.wishersPairs = [...this.wishers].sort(() => Math.random() - 0.5);
+    this.wishersPairsInput = this.wishersPairs.join('\n');
+  }
 
 }
